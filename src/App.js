9497420838287d0cde8ba82cd4c4a6cfe31eac62
Kevin Quesada Elizondo <kevinque01@gmail.com>
2021-08-30 23:22:34 -0600

    Add Authentication imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { AuthProvider } from './context/authContext';
 import Signup from './components/Authentication/Signup';
+import Login from './components/Authentication/Login';
+import ForgotPassword from './components/Authentication/ForgotPassword'
 
 function App() {
   return (
     <AuthProvider>
-      <Signup />
+      
     </AuthProvider>
   );
 }
