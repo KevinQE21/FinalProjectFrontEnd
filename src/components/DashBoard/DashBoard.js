57bbdc2dd2b363cdbf74e80e3fea9809a8401c67
Kevin Quesada Elizondo <kevinque01@gmail.com>
2021-08-31 20:37:45 -0600

    Add app DashBoard *Is Empty*

---
+++ src/components/DashBoard/DashBoard.js
@@ -0,0 +1,11 @@
+import NavigationBar from "../NavBar/NavigationBar";
+
+function Dashboard(){
+    return (
+        <>
+            <NavigationBar />
+        </>
+    )
+}
+
+export default Dashboard;
