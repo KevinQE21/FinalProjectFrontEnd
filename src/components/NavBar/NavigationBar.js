30aa2e6c4cf83b77014fb5d7e507c4544fdb22ce
Kevin Quesada Elizondo <kevinque01@gmail.com>
2021-09-01 22:08:22 -0600

    Minor changes

--- src/components/NavBar/NavigationBar.js
+++ src/components/NavBar/NavigationBar.js
@@ -1,10 +1,29 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../../context/authContext";
-import { HiCode, HiOutlineLogin } from 'react-icons/hi';
+import { HiCode, HiOutlineLogin, HiCog } from 'react-icons/hi';
+import { useState } from 'react';
 
 function NavigationBar(){
-    const { currentUser } = useAuth();
+    const { currentUser, logout } = useAuth();
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState('');
+    const history = useHistory(); 
+
+    async function handleLogout(e){
+        e.preventDefault();
+
+        try {
+            setError('');
+            setLoading(true);
+            await logout();
+            history.push('/');
+        }
+        catch (e){
+            setError(e.message);
+            setLoading(false);
+        }
+    }
 
     return (
         <Navbar bg="dark" variant="dark" expand="sm">
@@ -12,16 +31,22 @@ function NavigationBar(){
                 <Navbar.Brand as={ Link } to="/">Code News <HiCode /> </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                    {
-                    currentUser
-                    ? 
-                    <>                        
-                        <Navbar.Brand as={ Link } to="/">Profile <HiOutlineLogin /> </Navbar.Brand>
-                    </>
-                    : <Nav.Link as={ Link } to="/signup">Sign Up</Nav.Link>
-                    }
-                </Nav>
+                    <Nav className="ms-auto">
+                        {
+                        currentUser
+                        ? 
+                        <>
+                            <Nav.Link as={ Link } to="/profile">
+                                Profile <HiCog/>               
+                            </Nav.Link>
+                            <Nav.Link as={ Link } to="/login" onClick={handleLogout}>
+                                Logout <HiOutlineLogin/>               
+                            </Nav.Link>
+                        </>
+                        :
+                        <Nav.Link as={ Link } to="/signup">Sign Up</Nav.Link>
+                        }
+                    </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>        
