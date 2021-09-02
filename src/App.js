0167a1decab9784818d95e3934f0535f49757777
Kevin Quesada Elizondo <kevinque01@gmail.com>
2021-09-01 22:05:04 -0600

    Add routes to profile components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
+import { Route, BrowserRouter as Router, Redirect, Switch } from 'react-router-dom';
+
 import { AuthProvider } from './context/authContext';
+
 import Signup from './components/Authentication/Signup';
 import Login from './components/Authentication/Login';
-import ForgotPassword from './components/Authentication/ForgotPassword'
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import ForgotPassword from './components/Authentication/ForgotPassword';
 import Dashboard from './components/DashBoard/DashBoard';
+import Profile from './components/Profile/Profile';
+import PrivateRoute from './context/PrivateRoute';
+import UpdateEmail from './components/Profile/UpdateEmail';
+import UpdatePassword from './components/Profile/UpdatePassword';
 
 function App() {
+
   return ( 
     <AuthProvider>
       <Router>
-        <Route path="/" component={ Dashboard } />
-        <Route path="/signup" component={ Signup } />
-        <Route path="/login" component={ Login } />
-        <Route path="/forgot-password" component={ ForgotPassword } />
+        <Switch>
+          <Route exact path="/" component={ Dashboard } />
+          <PrivateRoute exact path="/profile" component={ Profile } />
+          <PrivateRoute exact path="/update-email" component={ UpdateEmail } />  
+          <PrivateRoute exact path="/update-password" component={ UpdatePassword } />                    
+          <Route path="/signup" component={ Signup } />
+          <Route exact path="/login" component={ Login } />
+          <Route path="/forgot-password" component={ ForgotPassword } />
+        </Switch>                  
       </Router>      
     </AuthProvider>    
   );
